ffc24ec0d62a7bb4b727fdbce171f8966a12b117
Derek Weitzel <dweitzel@cse.unl.edu>
2014-05-09 11:25:22 -0500

    Adding second RRD for pki traffic

--- jobs/updatenetwork.rb
+++ jobs/updatenetwork.rb
@@ -1,27 +1,38 @@
-# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '6m', :first_in => 0 do |job|
-
+def getRRD(url)
   require 'tempfile'
-  require 'rrd'
-  require 'net/http' 
- 
+  require 'net/http'
+  
   # First, create a temporary file
-  tmp_file = Tempfile.new('networkrrd')
-
-  # Download the RRD
-  rrd_url = "https://red-mon.unl.edu/cacti/rra/hcc-schorr-mlxe_traffic_in_836.rrd"
-  uri = URI(rrd_url)
-  req = Net::HTTP::Get.new(uri.path)
-
+  tmp_file = Tempfile.new('rrd') 
+  uri = URI(url)
+  req = req = Net::HTTP::Get.new(uri.path)
   response = Net::HTTP.start(uri.host, uri.port, :use_ssl => uri.scheme == 'https', :verify_mode => OpenSSL::SSL::VERIFY_NONE) do |https|
     https.request(req)
   end
 
   tmp_file.write(response.body)
+
+  return tmp_file
+
+end
+
+
+# :first_in sets how long it takes before the job is first run. In this case, it is run immediately
+SCHEDULER.every '6m', :first_in => 0 do |job|
+
+  require 'rrd'
+  
+  # Download the RRD
+  rrd_tmp = getRRD("https://red-mon.unl.edu/cacti/rra/hcc-schorr-mlxe_traffic_in_836.rrd")
+  rrd2_tmp = getRRD("https://red-mon.unl.edu/cacti/rra/hcc-pki-mlxe_traffic_in_1004.rrd")
+  
   
   # Read in the RRD, parse into points data structure
-  rrd = RRD::Base.new(tmp_file.path)
-  
+  rrd = RRD::Base.new(rrd_tmp.path)
+  rrd2 = RRD::Base.new(rrd2_tmp.path)
+ 
+  #network_debug = File.open("/tmp/networkdebug", 'w') 
+
   points = []
   last_point = 0
   min_point = 0
@@ -34,8 +45,31 @@ SCHEDULER.every '6m', :first_in => 0 do |job|
        last_point = (line[1].to_f + line[2].to_f)*8
      end
   end
-  tmp_file.close
-  tmp_file.unlink
+
+  counter = 0
+  rrd2.fetch(:average).each do |line|
+     if line[1].class != String && line[1].nan? != true
+       if min_point == 0
+         min_point = line[0].to_f
+       end
+       counter = 0
+       while points[counter][:x] != line[0].to_f do
+         #network_debug.puts "Incrementing counter #{points[counter][:x]} != #{line[0].to_f}" 
+         counter += 1
+       end
+       if points[counter][:x] == line[0].to_f 
+         #network_debug.puts "Found an equal: #{points[counter][:x]} = #{line[0].to_f}"
+       else
+         next
+       end
+       #network_debug.puts "Before add: #{points[counter][:y]}"
+       points[counter][:y] += ( line[1].to_f + line[2].to_f)*8
+       #network_debug.puts "After add: #{points[counter][:y]}"
+       last_point = points[counter][:y]
+     end
+  end
+  
+  #network_debug.close
 
   send_event('NetworkGraph', { points: points })
   send_event('HCCAmazonPrice', { network_bandwidth: last_point })
