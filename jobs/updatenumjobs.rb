49df19a53dc831b45d88fc973b8654cd3f1185bc
Derek Weitzel <dweitzel@cse.unl.edu>
2015-02-18 11:20:06 -0600

    Updating num jobs period to 30 seconds

--- jobs/updatenumjobs.rb
+++ jobs/updatenumjobs.rb
@@ -30,7 +30,7 @@ end
 
 
 # :first_in sets how long it takes before the job is first run. In this case, it is run immediately
-SCHEDULER.every '5s', :first_in => 0 do |job|
+SCHEDULER.every '30s', :first_in => 0 do |job|
   require 'time'
 
   # Previous 24 hours...
